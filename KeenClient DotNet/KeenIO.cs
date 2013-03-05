2b2e35c9fb3d11f032f864b977f53bde4ad5a7a5
firebellys <firebelly@gmail.com>
2013-03-05 15:31:51 -0800

    Initial Drop for GIT
    Signed-off-by: firebellys <firebelly@gmail.com>

---
+++ KeenClient DotNet/KeenIO.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeenClient_DotNet
+{
+    public class KeenIO
+    {
+        /// <summary>
+        /// Versionses this instance.
+        /// </summary>
+        public void Versions()
+        {
+            var temnpTest1 = new VersionResponse();
+
+
+        }
+        public void Discovery()
+        {
+        }
+        public void Projects()
+        {
+        }
+        public void ProjectRow()
+        {
+        }
+        public void Event()
+        {
+        }
+        public void EventCollection()
+        {
+        }
+        public void Queries()
+        {
+        }
+        public void Count()
+        {
+        }
+        public void CountUnique()
+        {
+        }
+        public void Minimum()
+        {
+        }
+        public void Maximum()
+        {
+        }
+        public void Average()
+        {
+        }
+        public void Sum()
+        {
+        }
+        public void SelectUnique()
+        {
+        }
+        public void Extraction()
+        {
+        }
+        public void FunnelResource()
+        {
+        }
+        public void SavedQueriesList()
+        {
+        }
+        public void SavedQueryRow()
+        {
+        }
+        public void SavedQuieryRowResult()
+        {
+        }
+    }
+
+    public class KeenUser
+    {
+        public string APIKEY { get; set; }
+
+    }
+}
---
+++ KeenClient DotNet/KeenResponses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeenClient_DotNet
+{
+    class KeenResponses
+    {
+    }
+    public class VersionResponse
+    {
+        public static string IsPublic { get; set; }
+        public static string url { get; set; }
+        public static string version { get; set; }
+    }
+
+
+}
